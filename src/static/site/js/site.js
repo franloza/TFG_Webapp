00c3ace5d57c35246b58bea3ab076c470edeeaa1
Fran Lozano <franloza@ucm.es>
2017-05-02 17:24:49 +0200

    Added fake loading bar

--- src/static/site/js/site.js
+++ src/static/site/js/site.js
@@ -21,3 +21,29 @@ $(".btn-group :input").change(function() {
       label.removeClass('active');
    }
 });
+
+$('input[name=generate_report]').click(function() {
+  $('div.progress').show();
+  $( this ).hide();
+  startLoadingBar();
+});
+
+$( window ).load(function() {
+  $('div.progress').hide()
+});
+
+
+function startLoadingBar() {
+  var elem = $('div.progress-bar-success')[0];
+  var width = 20;
+  elem.style.width = width + '%';
+  var id = setInterval(frame, 10000);
+  function frame() {
+    if (width == 100) {
+      clearInterval(id);
+    } else {
+      width += 10;
+      elem.style.width = width + '%';
+    }
+  }
+}
